eb4514ea3d393349fd88108a45eb3aa47d9ce609
smarty1palak <smarty1palak@gmail.com>
2018-10-18 06:08:54 +0530

    trying to convert dict to json

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 import Hybrid
 from flask import render_template
+from flask import jsonify, json
+
 app = Flask(__name__)
 
 
@@ -14,6 +16,10 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     data= Hybrid.hybrid(text)
+    list = []
+    for key, value in data.items():
+    	list.append({key:value})
+    data = jsonify(json.dumps(list))
     return render_template('result.html', data=data)
 
 if __name__ == '__main__':
