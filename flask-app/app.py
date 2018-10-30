d9299847de074b827c4576211790c63cd904be54
smarty1palak <smarty1palak@gmail.com>
2018-10-31 01:01:20 +0530

    sequential text

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask import request
 import Hybrid
 from flask import render_template
-from flask import jsonify, json
-
 app = Flask(__name__)
 
 
@@ -16,10 +14,6 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     data= Hybrid.hybrid(text)
-    list = []
-    for key, value in data.items():
-    	list.append({key:value})
-    data = jsonify(json.dumps(list))
     return render_template('result.html', data=data)
 
 if __name__ == '__main__':
