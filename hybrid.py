a0d0e4f4b881258d724a18aa3bcd3e2168bceb6b
smarty1palak <smarty1palak@gmail.com>
2018-11-11 13:14:49 +0530

    wiener attack resistant

--- hybrid.py
+++ hybrid.py
@@ -6,6 +6,30 @@ import random
 import hashlib
 import time
 import matplotlib.pyplot as plt 
+import ContinuedFractions, Arithmetic, RSAvulnerableKeyGenerator
+
+def hack_RSA(e,n):
+    '''
+    Finds d knowing (e,n)
+    applying the Wiener continued fraction attack
+    '''
+    frac = ContinuedFractions.rational_to_contfrac(e, n)
+    convergents = ContinuedFractions.convergents_from_contfrac(frac)
+    
+    for (k,d) in convergents:
+        
+        #check if d is actually the key
+        if k!=0 and (e*d-1)%k == 0:
+            phi = (e*d-1)//k
+            s = n - phi + 1
+            # check if the equation x^2 - s*x + n = 0
+            # has integer roots
+            discr = s*s - 4*n
+            if(discr>=0):
+                t = Arithmetic.is_perfect_square(discr)
+                if t!=-1 and (s+t)%2==0:
+                    print("Hacked!")
+                    return d
 # function to find extended gcd
 def egcd(a, b):
 	if a == 0:
@@ -140,6 +164,22 @@ if __name__ == '__main__':
 		print ("n: ", n)
 		x.append(n)
 		lis.append(time.time()- start_time)
+
+		times = 5
+		while(times>0):
+			e = public
+			d = private
+			print ("d = ", d)
+			hacked_d = hack_RSA(e, p*q)
+			if d == hacked_d:
+				print ("Hack WORKED!")
+			else:
+				print ("Hack FAILED")
+			print ("d = ", d, ", hacked_d = ", hacked_d)
+			print ("-------------------------")
+			times -= 1
+            
+
 		n= n+1
 
 	plt.plot(x, lis)
--- rsa.py
+++ rsa.py
@@ -1,12 +1,35 @@
 import random
 import time
 import matplotlib.pyplot as plt 
-
+import ContinuedFractions, Arithmetic, RSAvulnerableKeyGenerator
 
 '''
 Euclid's algorithm for determining the greatest common divisor
 Use iteration to make it faster for larger integers
 '''
+def hack_RSA(e,n):
+    '''
+    Finds d knowing (e,n)
+    applying the Wiener continued fraction attack
+    '''
+    frac = ContinuedFractions.rational_to_contfrac(e, n)
+    convergents = ContinuedFractions.convergents_from_contfrac(frac)
+    
+    for (k,d) in convergents:
+        
+        #check if d is actually the key
+        if k!=0 and (e*d-1)%k == 0:
+            phi = (e*d-1)//k
+            s = n - phi + 1
+            # check if the equation x^2 - s*x + n = 0
+            # has integer roots
+            discr = s*s - 4*n
+            if(discr>=0):
+                t = Arithmetic.is_perfect_square(discr)
+                if t!=-1 and (s+t)%2==0:
+                    print("Hacked!")
+                    return d
+
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
@@ -189,12 +212,12 @@ if __name__ == '__main__':
     '''
     lis = []
     x= []
-    n=1
-    while n<11:
+    n=3
+    while n<7:
         start_time = time.time()
         print "RSA Encrypter/ Decrypter"
         p,q = generateprime(2**n,2**(n+1))
-        print(p,q)
+        print p,q
         public, private = generatekey(p,q)
         print "Your public key is ", public ," and your private key is ", private
         '''message = raw_input("Enter a message to encrypt with your private key: ")'''        
@@ -206,17 +229,34 @@ if __name__ == '__main__':
         print "Decrypting message with public key ", public ," . . ."
         print "Your message is:"
         print decrypt(public, encrypted_msg)
-        print("--- %s seconds ---" % (time.time() - start_time))
+        print "--- %s seconds ---" % (time.time() - start_time)
         print "n: ", n
         x.append(n)
         lis.append(time.time()- start_time)
+        times = 5
+        while(times>0):
+            e,n1= public
+            d, n1= private
+            print "d = ", d
+        
+            hacked_d = hack_RSA(e, p*q)
+        
+            if d == hacked_d:
+                print "Hack WORKED!"
+            else:
+                print "Hack FAILED"
+            
+            print "d = ", d, ", hacked_d = ", hacked_d
+            print "-------------------------"
+            times -= 1
         n= n+1
+
     plt.plot(x, lis)
     plt.xlabel('Number of bits ')
     plt.ylabel('Time taken')
     plt.title('Bits in prime number vs time taken') 
     plt.show() 
-    plt.savefig('rsa-graph')
+    
 
 
 
